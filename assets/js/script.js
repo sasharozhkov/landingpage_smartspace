2aa413a164861a72a1189fb7b01cfb78eab9c47e
sasharozhkov <alexander_rozhkov@icloud.com>
2022-11-19 20:55:11 +0300

    feat: add section deals

--- assets/js/script.js
+++ assets/js/script.js
@@ -80,3 +80,25 @@ function myFunction() {
 }
 
 
+// tab
+const tabNavItems = document.querySelectorAll('.tabs-deals__button');
+const tabItems = document.querySelectorAll('.item-tabs');
+document.addEventListener("click", function (e) {
+    const targetElement = e.target;
+    let currentActiveIndex = null;
+    let newActiveIndex = null;
+    if (targetElement.closest('.tabs-deals__button')) {
+        tabNavItems.forEach((tabNavItem, index) => {
+            if (tabNavItem.classList.contains('active')) {
+                currentActiveIndex = index;
+                tabNavItem.classList.remove('active');
+            }
+            if (tabNavItem === targetElement) {
+                newActiveIndex = index;
+            }
+        });
+        targetElement.classList.add('active');
+        tabItems[currentActiveIndex].classList.remove('active');
+        tabItems[newActiveIndex].classList.add('active');
+    }
+})
